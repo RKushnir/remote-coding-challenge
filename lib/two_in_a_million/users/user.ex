1e84409105a558e5dd1866399c4c92659634204a
RKushnir <RKushnir@users.noreply.github.com>
2021-12-13 11:35:40 +0100

    Create the users table

--- lib/two_in_a_million/users/user.ex
+++ lib/two_in_a_million/users/user.ex
@@ -1,5 +1,15 @@
 defmodule TwoInAMillion.Users.User do
+  @moduledoc """
+  User is a participant in a lottery, where a winner is chosen based on their points.
+  """
+
+  use Ecto.Schema
+
   @derive {Jason.Encoder, only: [:id, :points]}
 
-  defstruct [:id, :points, :inserted_at, :updated_at]
+  schema "users" do
+    field :points, :integer
+
+    timestamps()
+  end
 end
---
+++ priv/repo/migrations/20211211175109_create_users.exs
@@ -0,0 +1,11 @@
+defmodule TwoInAMillion.Repo.Migrations.CreateUsers do
+  use Ecto.Migration
+
+  def change do
+    create table(:users) do
+      add(:points, :integer, null: false, default: 0)
+
+      timestamps()
+    end
+  end
+end
